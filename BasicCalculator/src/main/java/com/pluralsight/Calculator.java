25cc90393dff98a2e1e0cd04af1f60a825ecaaea
Jrobinson718 <jrobinson6284@gmail.com>
2025-04-24 11:29:42 -0400

    Refactor: Changing my code to throw InputMismatchExceptions should the user input an invalid operator or number using try/catch. Also uses a do/while loop to keep the user in the calculator until they provide valid inputs.

--- BasicCalculator/src/main/java/com/pluralsight/Calculator.java
+++ BasicCalculator/src/main/java/com/pluralsight/Calculator.java
@@ -1,24 +1,94 @@
 package com.pluralsight;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[]args){
+    static Scanner scanner = new Scanner(System.in);
 
-        Scanner LetsDoMath = new Scanner(System.in);
+    public static void main(String[] args) {
 
-        System.out.println("Enter the first number: ");
-        double number1 = LetsDoMath.nextDouble();
+        letsDoMath();
 
-        System.out.println("Enter the second number: ");
-        double number2 = LetsDoMath.nextDouble();
-        LetsDoMath.nextLine();
 
-        System.out.println("What do you want to do?");
-        System.out.println("     Enter \"A\" for addition, \"S\" for subtraction, \"M\" for multiplication or \"D\" for division:  ");
-        LetsDoMath.nextLine();
+    }
 
-        double product = number1 * number2;
-        System.out.printf("The product of %.0f and %.0f is %.0f", number1, number2, product);
+    public static void letsDoMath() {
+
+        try {
+            String operator;
+            String operation;
+            double result = 0;
+            double num1;
+            double num2;
+            boolean validNumbers = false;
+            boolean validOperator = false;
+            do {
+                try {
+                    System.out.print("Please enter a number: ");
+                    num1 = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    System.out.print("Please enter the second number: ");
+                    num2 = scanner.nextDouble();
+                    scanner.nextLine();
+                    validNumbers = true;
+
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid number\n");
+                    scanner.nextLine();
+                    continue;
+
+                }
+
+                System.out.print("People pick an operator symbol.\n" +
+                        "\"+\"  -  Addition\n" +
+                        "\"-\"  -  Subtraction\n" +
+                        "\"*\"  -  Multiplication\n" +
+                        "\"/\"  -  Division\n");
+                operator = scanner.nextLine().trim();
+                operation = operator;
+
+                if (operator.equals("+") || operator.equals("-")
+                        || operator.equals("*") || operator.equals("/")) {
+                    validOperator = true;
+                    switch (operator) {
+                        case "+" -> {
+                            result = num1 + num2;
+                            operation = "sum";
+                        }
+                        case "-" -> {
+                            result = num1 - num2;
+                            operation = "remainder";
+                        }
+                        case "*" -> {
+                            result = num1 * num2;
+                            operation = "product";
+                        }
+                        case "/" -> {
+                            if(num2 == 0){
+                                System.out.println("Divide by 0 error.");
+                                validOperator = false;
+
+                            }else {
+                                result = num1 / num2;
+                                operation = "quotient";
+
+                            }
+                        }
+                    }
+                    System.out.printf("The %s of %.2f and %.2f is %.2f", operation, num1, num2, result);
+                    scanner.close();
+
+                } else {
+                    System.out.println("Invalid operator\n");
+
+                }
+
+            } while (!validNumbers || !validOperator);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+
+        }
     }
 }
